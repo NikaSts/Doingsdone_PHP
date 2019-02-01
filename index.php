c8becb819302cd7caa10e16a5e475350eacc9651
Nefila <33237141+NikaSts@users.noreply.github.com>
2019-02-01 19:23:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -78,22 +78,21 @@ $show_complete_tasks = rand(0, 1);
                     <label class="checkbox">
                         <!--добавить сюда аттрибут "checked", если переменная $show_complete_tasks равна единице-->
                         <input class="checkbox__input visually-hidden show_completed" type="checkbox"
-                            <?php
-                            if ($show_complete_tasks == 1) {
-                                echo '<tr class="tasks__item task task--completed">
-    <td class="task__select">
-        <label class="checkbox task__checkbox">
-            <input class="checkbox__input visually-hidden" type="checkbox" checked>
-            <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
-        </label>
-    </td>
-    <td class="task__date">10.10.2019</td>
+                            <? if ($show_complete_tasks === 1): ?> checked <? endif; ?>
+                        <? if ($show_complete_tasks === 1): ?>
+                            <tr class="tasks__item task task--completed">
+                                <td class="task__select">
+                                    <label class="checkbox task__checkbox">
+                                        <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                                        <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+                                    </label>
+                                </td>
+                                <td class="task__date">10.10.2019</td>
 
-    <td class="task__controls">
-    </td>
-</tr>';
-                            }
-                            ?>>
+                                <td class="task__controls">
+                                </td>
+                            </tr>
+                        <? endif; ?>>
 
                         <span class="checkbox__text">Показывать выполненные</span>
                     </label>
