706c8cf12df50c14f699d6a3bbc53bf9a3393946
Nefila <33237141+NikaSts@users.noreply.github.com>
2019-02-10 11:49:27 +0100

    Создание схемы БД - правки

--- schema.sql
+++ schema.sql
@@ -4,23 +4,33 @@ CREATE DATABASE doingsdone
 USE doingsdone;
 CREATE TABLE users (
   id        INT AUTO_INCREMENT PRIMARY KEY,
-  signed_up TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  SIGNED_UP TIMESTAMP DEFAULT current_timestamp NOT NULL,
   email     CHAR(128) NOT NULL UNIQUE,
   name      CHAR(64)  NOT NULL UNIQUE,
   password  CHAR(64)  NOT NULL
 );
 CREATE TABLE projects (
   id      INT AUTO_INCREMENT PRIMARY KEY,
-  user_id INT
+  user_id INT NOT NULL
 );
 CREATE TABLE tasks (
   id         INT AUTO_INCREMENT PRIMARY KEY,
   name       CHAR(128) NOT NULL,
   project_id INT,
-  now_status TINYINT   DEFAULT 0,
-  file_link  CHAR(128),
+  now_status ENUM('0', '1') DEFAULT '0',
+  file_link  CHAR,
   time_limit TIMESTAMP,
-  is_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  is_done    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  user_id    INT
+  is_created DATETIME NOT NULL,
+  is_done    DATETIME,
+  user_id    INT NOT NULL
 );
+CREATE INDEX users_signed_up ON users(signed_up);
+CREATE UNIQUE INDEX users_email ON users(email);
+CREATE UNIQUE INDEX users_name ON users(name);
+CREATE INDEX users_password ON users(password);
+CREATE INDEX name ON tasks(name);
+CREATE INDEX tasks_now_status ON tasks(now_status);
+CREATE INDEX tasks_time_limit ON tasks(time_limit);
+CREATE INDEX tasks_is_created ON tasks(is_created);
+CREATE INDEX tasks_is_done ON tasks(is_done);
+
