c99d1fc490a5d3cebd76cd22e6b57d363e3bf53d
Nefila <33237141+NikaSts@users.noreply.github.com>
2019-02-11 20:44:24 +0100

    правки

--- schema.sql
+++ schema.sql
@@ -6,31 +6,33 @@ CREATE TABLE users (
   id        INT AUTO_INCREMENT PRIMARY KEY,
   SIGNED_UP TIMESTAMP DEFAULT current_timestamp NOT NULL,
   email     CHAR(128) NOT NULL UNIQUE,
-  name      CHAR(64)  NOT NULL UNIQUE,
-  password  CHAR(64)  NOT NULL
+  name      CHAR(64) NOT NULL UNIQUE,
+  password  CHAR(64) NOT NULL
 );
 CREATE TABLE projects (
   id      INT AUTO_INCREMENT PRIMARY KEY,
+  name    CHAR(30) NOT NULL UNIQUE,
   user_id INT NOT NULL
 );
 CREATE TABLE tasks (
   id         INT AUTO_INCREMENT PRIMARY KEY,
   name       CHAR(128) NOT NULL,
   project_id INT,
-  now_status ENUM('0', '1') DEFAULT '0',
+  now_status ENUM ('0', '1') DEFAULT '0',
   file_link  CHAR,
   time_limit TIMESTAMP,
   is_created DATETIME NOT NULL,
   is_done    DATETIME,
   user_id    INT NOT NULL
 );
-CREATE INDEX users_signed_up ON users(signed_up);
-CREATE UNIQUE INDEX users_email ON users(email);
-CREATE UNIQUE INDEX users_name ON users(name);
-CREATE INDEX users_password ON users(password);
-CREATE INDEX name ON tasks(name);
-CREATE INDEX tasks_now_status ON tasks(now_status);
-CREATE INDEX tasks_time_limit ON tasks(time_limit);
-CREATE INDEX tasks_is_created ON tasks(is_created);
-CREATE INDEX tasks_is_done ON tasks(is_done);
+CREATE INDEX users_signed_up ON users (signed_up);
+CREATE UNIQUE INDEX users_email ON users (email);
+CREATE UNIQUE INDEX users_name ON users (name);
+CREATE INDEX users_password ON users (password);
+CREATE INDEX tasks_name ON tasks (name);
+CREATE UNIQUE INDEX projects_name ON projects (name);
+CREATE INDEX tasks_now_status ON tasks (now_status);
+CREATE INDEX tasks_time_limit ON tasks (time_limit);
+CREATE INDEX tasks_is_created ON tasks (is_created);
+CREATE INDEX tasks_is_done ON tasks (is_done);
 
