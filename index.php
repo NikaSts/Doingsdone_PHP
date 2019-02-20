2ddfdf37fc4203c9616d50160e8115cbf47596d4
Nefila <33237141+NikaSts@users.noreply.github.com>
2019-02-20 14:16:20 +0100

    Задание 12 - Работа со строкой запроса

--- index.php
+++ index.php
@@ -4,27 +4,56 @@ $show_complete_tasks = rand(0, 1);
 
 require_once 'init.php';
 
-if (!$connect) {
-    print('Невозможно подключиться к базе данных: ' . mysqli_connect_error());
-} else {
-    $sql1 = "SELECT * FROM projects";
-    $sql2 = "SELECT * FROM tasks";
+print_r($_GET);
 
-    $projects = db_fetch_data($connect, $sql1);
-    $tasks = db_fetch_data($connect, $sql2);
+$error = [];
+
+if (!$connect) {
+    $error = 'Невозможно подключиться к базе данных: ' . mysqli_connect_error();
+} else {
+    $user_id = 1;
+    $sql_projects = "SELECT * FROM projects WHERE user_id = ?";
+    $projects = db_fetch_data($connect, $sql_projects, [$user_id]);
+
+    $tasks = [];
+    $project_id = intval($_GET['project_id']);
+
+    $sql_tasks = "SELECT * FROM tasks WHERE user_id = ?";
+    $tasks = db_fetch_data($connect, $sql_tasks, [$user_id]);
+
+    if (isset($_GET['project_id']) && !$project_id) {
+        $error = '404';
+    } else if ($project_id) {
+        $sql_tasks_count = "SELECT COUNT(*) as count FROM tasks WHERE user_id = ? AND project_id = ?";
+        $task_count = db_fetch_data($connect, $sql_tasks_count, [$user_id, $project_id]);
+        if (!$task_count[0]['count']) {
+            $error = '404';
+        }
+    }
 }
-$page_content = include_template('index.php', [
-    'show_complete_tasks' => $show_complete_tasks,
-    'tasks' => $tasks
-]);
+
+if ($error) {
+    http_response_code(404);
+    $page_content = include_template('error.php', [
+        'error' => $error
+    ]);
+} else {
+    http_response_code(200);
+    $page_content = include_template('index.php', [
+        'show_complete_tasks' => $show_complete_tasks,
+        'tasks' => $tasks
+    ]);
+}
 
 $layout_content = include_template('layout.php', [
     'page_content' => $page_content,
     'projects' => $projects,
     'tasks' => $tasks,
-    'title' => 'Дела в порядке'
+    'title' => 'Дела в порядке',
 ]);
+
 print($layout_content);
+
 
 
 
