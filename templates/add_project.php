45121bef9595a09efa1f0e7719b81b5ce7571688
Nefila <33237141+NikaSts@users.noreply.github.com>
2019-02-25 11:13:24 +0100

    Update add_project.php

--- templates/add_project.php
+++ templates/add_project.php
@@ -2,9 +2,6 @@
     <h2 class="content__main-heading">Добавление проекта</h2>
 
     <form class="form"  action="/add_project.php" method="post">
-        <? if (isset($errors)): ?>
-            <p class="error-message">Пожалуйста, исправьте ошибки в форме</p>
-        <? endif; ?>
         <div class="form__row">
             <label class="form__label" for="project_name">Название <sup>*</sup></label>
             <input class="form__input <?= isset($errors['name']) ? " form__input--error" : '' ?>" type="text" name="name" id="project_name" value="<?= isset($_POST['name']) ? esc($_POST['name']) : ''; ?>" placeholder="Введите название проекта">
