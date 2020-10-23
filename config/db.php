a34c972ddfbe6fa6e86a388cd99abb17b27fb3c4
Nefila <veronika.sts@gmail.com>
2020-10-23 19:28:37 +0200

    deploy to heroku

--- config/db.php
+++ config/db.php
@@ -1,9 +1,10 @@
 <?php
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
 
 $db = [
-    'host' => 'localhost',
-    'user' => 'mysql',
-    'password' => 'mysql',
-    'database' => 'doingsdone'
+    'host' => $dbparts['host'],
+    'user' => $dbparts['user'],
+    'password' => $dbparts['pass'],
+    'database' => ltrim($dbparts['path'],'/')
 ];
-
